9278a48815fe95ffb8626023895868bc64abac0d
Steven Eastcott <sdeastcott@crimson.ua.edu>
2016-12-19 23:47:04 -0500

    angular 2 project

---
+++ grouplytics-web/src/app/result-page/group-list-result.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-list-result',
+  templateUrl: './group-list-result.component.html'
+})
+export class GroupListResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ grouplytics-web/src/app/select-page/group-select.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-select',
+  templateUrl: './group-select.component.html'
+})
+export class GroupSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
