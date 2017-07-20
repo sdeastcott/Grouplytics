69f6e7ef04bed61cddb9898794611c18ec6ea27d
Steven Eastcott <sdeastcott@crimson.ua.edu>
2017-07-19 21:28:26 -0500

    removed angular project, shifted around files

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, session
+from flask import Flask, request, jsonify, session, render_template
 from src.grouplytics import Grouplytics
 from src.groupme_wrapper import GroupMeWrapper
 app = Flask(__name__)
@@ -7,6 +7,9 @@ SECRET_KEY = "development key"
 app.config.from_object(__name__)
 app.config.from_envvar("APPLICATION_CONFIG", silent=True)
 
+@app.route('/')
+def home():
+    return render_template('login.html')
 
 @app.route('/callback')
 def receive_token():
@@ -36,67 +39,6 @@ def all_reports():
             grouplytics.dude_report(),
             grouplytics.images_shared()
         ])
-
-@app.route('/overall_message_report', methods=["POST"])
-def overall_message_report():
-    data = request.get_json()
-    groupme = GroupMeWrapper(data['token'], data['name'], data['members'])
-    grouplytics = Grouplytics(groupme.members, groupme.messages)
-    return jsonify({'title': 'Overall Message Report',
-                    'report': grouplytics.overall_message_report()})
-
-
-@app.route('/swear_word_report', methods=["POST"])
-def swear_word_report():
-    data = request.get_json()
-    groupme = GroupMeWrapper(data['token'], data['name'], data['members'])
-    grouplytics = Grouplytics(groupme.members, groupme.messages)
-    return jsonify({'title': 'Swear Word Report',
-                    'report': grouplytics.swear_word_report()})
-
-
-@app.route('/avg_word_length', methods=["POST"])
-def avg_word_length():
-    data = request.get_json()
-    groupme = GroupMeWrapper(data['token'], data['name'], data['members'])
-    grouplytics = Grouplytics(groupme.members, groupme.messages)
-    return jsonify({'title': 'Average Word Length',
-                    'report': grouplytics.avg_word_length()})
-
-
-@app.route('/likes_received', methods=["POST"])
-def likes_received():
-    data = request.get_json()
-    groupme = GroupMeWrapper(data['token'], data['name'], data['members'])
-    grouplytics = Grouplytics(groupme.members, groupme.messages)
-    return jsonify({'title': 'Likes Received',
-                    'report': grouplytics.likes_received()})
-
-
-@app.route('/messages_liked', methods=["POST"])
-def messages_liked():
-    data = request.get_json()
-    groupme = GroupMeWrapper(data['token'], data['name'], data['members'])
-    grouplytics = Grouplytics(groupme.members, groupme.messages)
-    return jsonify({'title': 'Messages Liked',
-                    'report': grouplytics.messages_liked()})
-
-
-@app.route('/images_shared', methods=["POST"])
-def meme_lord_report():
-    data = request.get_json()
-    groupme = GroupMeWrapper(data['token'], data['name'], data['members'])
-    grouplytics = Grouplytics(groupme.members, groupme.messages)
-    return jsonify({'title': 'Images Shared',
-                      'report': grouplytics.images_shared()})
-
-
-@app.route('/dude_report', methods=["POST"])
-def dude_report():
-    data = request.get_json()
-    groupme = GroupMeWrapper(data['token'], data['name'], data['members'])
-    grouplytics = Grouplytics(groupme.members, groupme.messages)
-    return jsonify(**{'title': 'Dude Report', 'report': grouplytics.dude_report()})
 
 
 if __name__ == "__main__":
